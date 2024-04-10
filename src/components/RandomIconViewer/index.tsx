947a32d94e5cde50088f77e034a7844194f2eef0
Alina Zaets <grinkevich.alina@gmail.com>
2024-04-10 18:10:48 +0200

    Add spinner and add typescript fixes

--- src/components/RandomIconViewer/index.tsx
+++ src/components/RandomIconViewer/index.tsx
@@ -1,31 +1,58 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import * as solidIcons from "@fortawesome/free-solid-svg-icons";
 import * as brandsIcons from "@fortawesome/free-brands-svg-icons";
 import * as regularIcons from "@fortawesome/free-regular-svg-icons";
 
 import "./styles.css";
 
+const extractIcons = (
+  iconsModule: typeof solidIcons | typeof brandsIcons | typeof regularIcons
+) => {
+  return Object.entries(iconsModule)
+    .filter(([key, _]) => /^fa[A-Z0-9].*/.test(key))
+    .map(([_, value]) => value) as IconDefinition[];
+};
 
 const ICONS = [
-  ...Object.values(solidIcons),
-  ...Object.values(brandsIcons),
-  ...Object.values(regularIcons),
+  ...extractIcons(solidIcons),
+  ...extractIcons(brandsIcons),
+  ...extractIcons(regularIcons),
 ];
 
 const getRandomIcon = () => ICONS[Math.floor(Math.random() * ICONS.length)];
 
 function RandomIconViewer() {
   const [icon, setIcon] = useState(() => getRandomIcon());
+  const [isLoading, setIsLoading] = useState(false);
 
-  const showRandomIcon = () => {
-    setTimeout(() => setIcon(getRandomIcon()), 3000);
-  };
+  const showRandomIcon = useCallback(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIcon(getRandomIcon());
+      setIsLoading(false);
+    }, 500);
+  }, []);
 
   return (
-    <div className="viewer-container">
-      <button onClick={() => showRandomIcon()}>Generate Icon</button>
-      <FontAwesomeIcon className="icon-svg" icon={icon} />
+    <div className="random-icon-viewer-container">
+      <button
+        onClick={() => showRandomIcon()}
+        className="random-icon-viewer__button"
+      >
+        Generate Icon
+      </button>
+      {isLoading ? (
+        <FontAwesomeIcon
+          icon={solidIcons.faSpinner}
+          spinPulse
+          size="2xl"
+          className="random-icon-viewer__spinner"
+        />
+      ) : (
+        <FontAwesomeIcon className="random-icon-viewer__icon" icon={icon} />
+      )}
     </div>
   );
 }
